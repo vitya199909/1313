8b17e6c85b0a1b15bb2f9b55b5ca4c186303c507
“Viktor <vitya199909@gmail.com>
2025-01-22 14:10:38 +0200

    dump files to git repository: 1.sh, https.sh

---
+++ Bash/1.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# MYVAR="DevOps"
+#echo "Hello! $MYVAR!"
+#echo 'Hello! $MYVAR!'
+#read MYVAR
+#echo "Hello $name"
+#echo "hello $0 $1 $2"
+#list= `ls -1`
+#echo $list
+
+#let x=10+2*4
+#echo "$((10+40))"
+
+#if [ expression ];
+#       then
+#               statements
+#       elif [ expression ];
+#       then
+#               statements
+#       else
+#               statements
+#       fi
+
+#echo -n "Enter your login name: "
+#read name
+#if [ "$name" = "$USER" ];
+#       then
+#               echo "Hello $name"
+#       else
+#               echo "you are not $USER, so who are you?"
+#       fi
+
+
+# for myfile in /etc/r*; do
+#   if [ -d "$myfile" ]
+#     then echo "$myfile (dir)"
+#   else
+#     echo "$myfile"
+#   fi
+# done
+
+
+# cut -d , -f 5 <<< "Website,Domain,DNS,SMTP,5005"
+
+#expr substr "458449SoftServe4132" 7 9
+
+#for i in {0..4}; do
+#    filename="file$i.txt"
+#    echo "$filename" > "$filename"
+#    echo "This is file $i. $filename." >> log.log
+#done
+
+#cat log.log
+
+
+
+
+
+#!/usr/bin/env bash
+#read name
+
+#cat $name
+
+
+
+#!/usr/bin/env bash
+#for word in "$@"; do
+#    echo "$word"
+#done
+
+
+
+#for i in {0..4}; do
+#    filename="file$i.txt"
+#    echo "$filename" > "$filename"
+#    echo "This is file $i. $filename" >> log.log
+#done
+#
+#cat log.log
